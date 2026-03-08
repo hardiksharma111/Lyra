40667239bc2c5d801c37919fbbcb8ecd9f7bef9a
Hardik Sharma <hardiksharma1031@gmail.com>
2026-03-08 10:28:07 +0530

    add cloud sync, drive scope, setup_google.py

--- tools/google_control.py
+++ tools/google_control.py
@@ -9,7 +9,8 @@ SCOPES = [
     'https://www.googleapis.com/auth/contacts.readonly',
     'https://www.googleapis.com/auth/gmail.readonly',
     'https://www.googleapis.com/auth/classroom.courses.readonly',
-    'https://www.googleapis.com/auth/classroom.student-submissions.me.readonly',
+    'https://www.googleapis.com/auth/classroom.student-submissions.me.readonly'
+    "https://www.googleapis.com/auth/drive.file",
 ]
 
 ACCOUNTS = {
